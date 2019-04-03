fdfaab6be69c27aea5fe66bdaa0ac1fe010e09a0
emilmerle <emilmerle8@gmail.com>
2019-04-03 11:38:14 +0200

    Anleitung

---
+++ C-letS-GO/alpha/anleitung.c
@@ -0,0 +1,12 @@
+		printf("How to play:\n");
+		printf("\t- player 1 moves with [W], [A], [S], [D] and places bombs with [Enter]\n");
+		printf("\t- player 2 moves with [ARROW_UP], [ARROW_LEFT], [ARROW-DOWN], [ARROW_RIGHT] and places bombs with [Space]\n");
+		printf(" \n");
+		printf("\t- Bombs can't destroy the solid blocks (#)\n");
+//        printf("Bombs can destroy the destructible blocks (x)\n");
+        printf("\t- Do not walk into the explosions!\n");
+        printf(" \n");
+        printf("\t- One game consists of several rounds\n");
+        printf("\t- A round will end when one player dies\n");
+        printf("\t- The first player to win 3 rounds wins the game\n");
+
